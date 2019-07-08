a4eb72a40a3704ae408bdb027ef6d79c4add1838
Juhwan Song <jsong785@gmail.com>
2019-07-07 21:51:11 -0600

    removed catch from repo

--- catch/catch-config.cmake
+++
@@ -1,5 +0,0 @@
-cmake_minimum_required(VERSION 3.5.1)
-project(SinglyLinkedList VERSION 1.0 LANGUAGES CXX)
-
-add_library(catch INTERFACE)
-target_include_directories(catch INTERFACE ${CMAKE_CURRENT_LIST_DIR})
