d0de7dd34f7c28fd947864d59cb49005186f42b2
Juhwan Song <jsong785@gmail.com>
2019-07-10 20:36:39 -0600

    hourglass workign

--- hourglass/HourGlass.cpp
+++ hourglass/HourGlass.cpp
@@ -2,9 +2,10 @@
 #include <assert.h>
 
 template
-<typename T, typename N>
-int GetHourGlassMaxSum(T (&matrix)[N][N])
+<typename T, int N>
+int GetHourglassMaxSum(T (&&matrix)[N][N])
 {
+    static_assert(N >= 3, "Matrix must be at least 3x3");
     const auto getVal = [](const auto matrix[N][N], const auto originRow, const auto originCol, const auto i) {
         switch(i) {
             case 0:
@@ -27,42 +28,103 @@ int GetHourGlassMaxSum(T (&matrix)[N][N])
         return 0;
     };
 
-    const auto canDoHourGlass = [](const auto x, const auto y) {
-        return x + 3 < N && y + 3 < N;
+    const auto canDoHourglass = [](const auto x, const auto y) {
+        return x + 2 < N && y + 2 < N;
     };
 
-    bool init{ false };
-    int max{ 0 };
+    const auto getMaxSumOfHourglass = [&canDoHourglass, &getVal](const auto matrix[N][N]){
+        bool init{ false };
+        int max{ 0 };
 
-    for(auto row{ 0 }; row < N; ++row) {
-        if(!canDoHourGlass(row, 0)) { break; } // this row is invalid, reached the end!
+        for(auto row{ 0 }; row < N; ++row) {
+            if(!canDoHourglass(row, 0)) { break; } // this row is invalid, reached the end!
 
-        for(auto col{ 0 }; col < N; ++col) {
-            if(!canDoHourGlass(0, col)) { break; } // this col is invalid, reached the end!
+            for(auto col{ 0 }; col < N; ++col) {
+                if(!canDoHourglass(0, col)) { break; } // this col is invalid, reached the end!
 
-            const auto sum{ getVal(matrix, row, col, 0) + getVal(matrix, row, col, 1) + getVal(matrix, row, col, 2) + 
-                                                          getVal(matrix, row, col, 3) + 
-                            getVal(matrix, row, col, 4) + getVal(matrix, row, col, 5) + getVal(matrix, row, col, 6) };
-            if(!init || sum > max) {
-                init = true;
-                max = sum;
+                const auto sum{ getVal(matrix, row, col, 0) + getVal(matrix, row, col, 1) + getVal(matrix, row, col, 2) + 
+                                                              getVal(matrix, row, col, 3) + 
+                                getVal(matrix, row, col, 4) + getVal(matrix, row, col, 5) + getVal(matrix, row, col, 6) };
+                if(!init || sum > max) {
+                    init = true;
+                    max = sum;
+                }
             }
         }
-    }
+        assert(init);
+        return max;
+    };
 
-    return max;
+    return getMaxSumOfHourglass(matrix);
 }
 
-TEST_CASE("HourGlass", "")
+TEST_CASE("Hourglass", "")
 {
-    SECTION("1")
+    // won't compile (templates!) (not a square matrix)
+    /*CHECK(GetHourglassMaxSum({ { 9, 9, 4},
+                               { 9, 4, 1},
+                               { 9, 9, 4},
+                               { 0, 0, 0} }) == 63);*/
+    // won't compile, minimum is 3x3 matrix
+    /*CHECK(GetHourglassMaxSum({ { 9, 9 },
+                               { 0, 0 } }) == 63);*/
+    SECTION("4x4")
     {
-        int matrix[4][4]{
-            { 1, 4, 4, 4},
-            { 1, 1, 4, 1},
-            { 1, 4, 4, 4},
-            { 1, 0, 0, 0}
-        }
-        CHECK(GetHourGlassMaxSum(matrix) == 24);
+        // upper left
+        CHECK(GetHourglassMaxSum({ { 9, 9, 9, 4},
+                                   { 1, 9, 4, 1},
+                                   { 9, 9, 9, 4},
+                                   { 1, 0, 0, 0} }) == 63);
+
+        // upper right
+        CHECK(GetHourglassMaxSum({ { 1, 4, 4, 4},
+                                   { 1, 1, 4, 1},
+                                   { 1, 4, 4, 4},
+                                   { 1, 0, 0, 0} }) == 28);
+
+        // lower left
+        CHECK(GetHourglassMaxSum({ { 1, 4, 4, 4},
+                                   { 5, 5, 5, 1},
+                                   { 1, 5, 4, 4},
+                                   { 5, 5, 5, 0} }) == 35);
+
+        // lower right
+        CHECK(GetHourglassMaxSum({ { 1, 4, 4, 4},
+                                   { 5, 8, 8, 8},
+                                   { 1, 5, 8, 4},
+                                   { 5, 8, 8, 8} }) == 56);
+    }
+
+    SECTION("10x10")
+    {
+        CHECK(GetHourglassMaxSum({ { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } }) == 0);
+
+        // in the middle somewhere
+        CHECK(GetHourglassMaxSum({ { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 9, 9, 9, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 9, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 9, 9, 9, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                                   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } }) == 63);
+    }
+
+    SECTION("3x3")
+    {
+        CHECK(GetHourglassMaxSum({ { 9, 9, 9 },
+                                   { 1, 9, 4 },
+                                   { 9, 9, 9 } }) == 63);
     }
 }
