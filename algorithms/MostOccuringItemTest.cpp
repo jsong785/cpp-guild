621446724129d11b063637a19cf880b1218c2c56
Juhwan Song <jsong785@gmail.com>
2019-07-13 15:27:47 -0600

    more changes

--- algorithms/MostOccuringItemTest.cpp
+++ algorithms/MostOccuringItemTest.cpp
@@ -1,5 +1,6 @@
 #include "catch.hpp"
 #include "MostOccuringItem.hpp"
+#include <list>
 #include <vector>
 
 TEST_CASE("most occuring", "[count]")
@@ -8,6 +9,11 @@ TEST_CASE("most occuring", "[count]")
     CHECK(count::MostOccuringItem(std::vector<int>{ 0, 1 }) == 1);
     CHECK(count::MostOccuringItem(std::vector<int>{ 0, 1, 1, 3, 5 }) == 1);
     CHECK(count::MostOccuringItem(std::vector<int>{ 0, 1, 1, 3, 5, 5 }) == 5);
-
     CHECK(count::MostOccuringItem(std::vector<int>{ 0, 1, 1, 3, 5, 5, 1 }) == 1);
+
+    CHECK(count::MostOccuringItem(std::list<int>{ 1 }) == 1);
+    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1 }) == 1);
+    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1, 1, 3, 5 }) == 1);
+    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1, 1, 3, 5, 5 }) == 5);
+    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1, 1, 3, 5, 5, 1 }) == 1);
 }
