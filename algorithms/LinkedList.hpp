c7890b97c04cd5c09309c718adc5ba4e1d232a86
Juhwan Song <jsong785@gmail.com>
2019-07-14 16:13:53 -0600

    linkedlist edits

--- algorithms/LinkedList.hpp
+++ algorithms/LinkedList.hpp
@@ -3,13 +3,70 @@
 namespace LinkedList {
 
 template <typename T>
-T* ReverseSinglyLinkedList(T* head) {
-    return head;
+T* ReverseSinglyLinkedListNonCircular(T* head) {
+    T* cache{ nullptr };
+    while(head != nullptr) {
+        auto currentHead{ head };
+        head = head->next;
+        currentHead->next = cache;
+        cache = currentHead;
+    }
+    return cache;
 }
 
 template <typename T>
-T* ReverseDoublyLinkedList(T* head) {
-    return head;
+T* ReverseSinglyLinkedListCircular(T* head) {
+    T* headCache{ head };
+    T* cache{ nullptr };
+    while(head != nullptr) {
+        auto currentHead{ head };
+        head = head->next;
+        currentHead->next = cache;
+        cache = currentHead;
+
+        if(currentHead->next == headCache) {
+            return currentHead;
+        }
+    }
+    return nullptr;
+}
+
+template <typename T>
+T* ReverseDoublyLinkedListNonCircular(T* head) {
+    while(head != nullptr) {
+        auto current{ head };
+        head = head->next;
+
+        auto next = current->next;
+        auto prev = current->prev;
+
+        current->prev = next;
+        current->next = prev;
+        
+        if(head == nullptr) {
+            return current;
+        }
+    }
+    return nullptr;
+}
+
+template <typename T>
+T* ReverseDoublyLinkedListCircular(T* head) {
+    auto headCache{ head };
+    while(head != nullptr) {
+        auto current{ head };
+        head = head->next;
+
+        auto next = current->next;
+        auto prev = current->prev;
+
+        current->prev = next;
+        current->next = prev;
+        if(current->prev == headCache) {
+            return current;
+        }
+    }
+    return nullptr;
 }
 
 }
--- algorithms/LinkedListTest.cpp
+++ algorithms/LinkedListTest.cpp
@@ -12,23 +12,61 @@ namespace {
     };
 }
 
-TEST_CASE("Singly Linked", "[LinkedList]")
-{
-    using namespace LinkedList;
-}
-
-TEST_CASE("Doubly Linked", "[LinkedList]")
+TEST_CASE("Singly Linked (non-circular)", "[LinkedList]")
 {
     using namespace LinkedList;
     SECTION("empty")
     {
-        CHECK(ReverseDoublyLinkedList(static_cast<Doubly*>(nullptr)) ==  nullptr);
+        CHECK(ReverseSinglyLinkedListNonCircular(static_cast<Singly*>(nullptr)) ==  nullptr);
+    }
+
+    SECTION("single")
+    {
+        Singly head;
+        CHECK(ReverseSinglyLinkedListNonCircular(&head) == &head);
+    }
+
+    SECTION("two")
+    {
+        Singly head;
+        Singly tail;
+
+        head.next = &tail;
+        CHECK(ReverseSinglyLinkedListNonCircular(&head) == &tail);
+
+        CHECK(tail.next == &head);
+        CHECK(head.next == nullptr);
+    }
+
+    SECTION("three")
+    {
+        Singly head;
+        Singly middle;
+        Singly tail;
+
+        head.next = &middle;
+        middle.next = &tail;
+
+        CHECK(ReverseSinglyLinkedListNonCircular(&head) == &tail);
+
+        CHECK(tail.next == &middle);
+        CHECK(middle.next == &head);
+        CHECK(head.next == nullptr);
+    }
+}
+
+TEST_CASE("Singly Linked (circular)", "[LinkedList]")
+{
+    using namespace LinkedList;
+    SECTION("empty")
+    {
+        CHECK(ReverseDoublyLinkedListNonCircular(static_cast<Doubly*>(nullptr)) ==  nullptr);
     }
 
     SECTION("single")
     {
         Doubly head;
-        CHECK(ReverseDoublyLinkedList(&head) == &head);
+        CHECK(ReverseDoublyLinkedListNonCircular(&head) == &head);
     }
 
     SECTION("two")
@@ -38,12 +76,150 @@ TEST_CASE("Doubly Linked", "[LinkedList]")
 
         head.next = &tail;
         tail.prev = &head;
-        CHECK(ReverseDoublyLinkedList(&head) == &tail);
+        CHECK(ReverseDoublyLinkedListNonCircular(&head) == &tail);
 
         CHECK(tail.next == &head);
         CHECK(head.next == nullptr);
 
         CHECK(head.prev == &tail);
         CHECK(tail.prev == nullptr);
+    }
+
+    SECTION("three")
+    {
+        Doubly head;
+        Doubly middle;
+        Doubly tail;
+
+        head.next = &middle;
+        middle.next = &tail;
+
+        tail.prev = &middle;
+        middle.prev = &head;
+        CHECK(ReverseDoublyLinkedListNonCircular(&head) == &tail);
+
+        CHECK(tail.next == &middle);
+        CHECK(middle.next == &head);
+        CHECK(head.next == nullptr);
+
+        CHECK(head.prev == &middle);
+        CHECK(middle.prev == &tail);
+        CHECK(tail.prev == nullptr);
+    }
+}
+
+TEST_CASE("Doubly Linked (non-circular)", "[LinkedList]")
+{
+    using namespace LinkedList;
+    SECTION("empty")
+    {
+        CHECK(ReverseDoublyLinkedListNonCircular(static_cast<Doubly*>(nullptr)) ==  nullptr);
+    }
+
+    SECTION("single")
+    {
+        Doubly head;
+        CHECK(ReverseDoublyLinkedListNonCircular(&head) == &head);
+    }
+
+    SECTION("two")
+    {
+        Doubly head;
+        Doubly tail;
+
+        head.next = &tail;
+        tail.prev = &head;
+        CHECK(ReverseDoublyLinkedListNonCircular(&head) == &tail);
+
+        CHECK(tail.next == &head);
+        CHECK(head.next == nullptr);
+
+        CHECK(head.prev == &tail);
+        CHECK(tail.prev == nullptr);
+    }
+
+    SECTION("three")
+    {
+        Doubly head;
+        Doubly middle;
+        Doubly tail;
+
+        head.next = &middle;
+        middle.next = &tail;
+
+        tail.prev = &middle;
+        middle.prev = &head;
+        CHECK(ReverseDoublyLinkedListNonCircular(&head) == &tail);
+
+        CHECK(tail.next == &middle);
+        CHECK(middle.next == &head);
+        CHECK(head.next == nullptr);
+
+        CHECK(head.prev == &middle);
+        CHECK(middle.prev == &tail);
+        CHECK(tail.prev == nullptr);
+    }
+}
+
+TEST_CASE("Doubly Linked (circular)", "[LinkedList]")
+{
+    using namespace LinkedList;
+    SECTION("empty")
+    {
+        CHECK(ReverseDoublyLinkedListCircular(static_cast<Doubly*>(nullptr)) ==  nullptr);
+    }
+
+    SECTION("single")
+    {
+        Doubly head;
+        head.next = &head;
+        head.prev = &head;
+
+        CHECK(ReverseDoublyLinkedListCircular(&head) == &head);
+        CHECK(head.next == &head);
+        CHECK(head.prev == &head);
+    }
+
+    SECTION("two")
+    {
+        Doubly head;
+        Doubly tail;
+
+        head.next = &tail;
+        tail.next = &head;
+
+        tail.prev = &head;
+        head.prev = &tail;
+        CHECK(ReverseDoublyLinkedListCircular(&head) == &tail);
+
+        CHECK(tail.next == &head);
+        CHECK(head.next == &tail);
+
+        CHECK(head.prev == &tail);
+        CHECK(tail.prev == &head);
+    }
+
+    SECTION("three")
+    {
+        Doubly head;
+        Doubly middle;
+        Doubly tail;
+
+        head.next = &middle;
+        middle.next = &tail;
+        tail.next = &head;
+
+        tail.prev = &middle;
+        middle.prev = &head;
+        head.prev = &tail;
+        CHECK(ReverseDoublyLinkedListCircular(&head) == &tail);
+
+        CHECK(tail.next == &middle);
+        CHECK(middle.next == &head);
+        CHECK(head.next == &tail);
+
+        CHECK(head.prev == &middle);
+        CHECK(middle.prev == &tail);
+        CHECK(tail.prev == &head);
     }
 }
