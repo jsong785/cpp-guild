a638fd3da063d841af6698e082acf5841ad4983b
Juhwan Song <jsong785@gmail.com>
2019-07-14 14:12:42 -0600

    most occuring sorted

--- algorithms/MostOccuringItem.hpp
+++ algorithms/MostOccuringItem.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <unordered_map>
 
 namespace count {
@@ -6,8 +7,6 @@ namespace count {
 // assuming not empty
 template <typename List> 
 constexpr typename List::value_type MostOccuringItem(List &&list) {
-    typename List::value_type ret{ 0 };
-
     std::unordered_map<typename List::value_type, unsigned> count;
     for(const auto &l : list) {
         auto found = count.find(l);
@@ -29,6 +28,49 @@ constexpr typename List::value_type MostOccuringItem(List &&list) {
     }
 
     return currentVal;
+}
+
+// assuming not empty
+template <typename List> 
+constexpr typename List::value_type MostOccuringItemSort(List &&list) {
+    std::sort(std::begin(list), std::end(list));
+
+    auto listIter{ std::begin(list) };
+    auto candidate{ *listIter };
+    auto candidateCount{ 1 };
+    ++listIter;
+    while(listIter != std::end(list) && *listIter == candidate) {
+        ++candidateCount;
+        ++listIter;
+    }
+
+    if(listIter == std::end(list)) {
+        return candidate;
+    }
+
+    auto current{ *listIter };
+    auto currentCount{ 1 };
+    ++listIter;
+    for(; listIter != std::end(list); ++listIter) {
+        if(*listIter == current) { 
+            ++currentCount;
+        }
+        else {
+            if(currentCount > candidateCount || (currentCount == candidateCount && current > candidate)){ 
+                candidate = current;
+                candidateCount = currentCount;
+            }
+            current = *listIter;
+            currentCount = 1;
+        }
+    }
+
+    if(currentCount > candidateCount || (currentCount == candidateCount && current > candidate)){ 
+        candidate = current;
+        candidateCount = currentCount;
+    }
+
+    return candidate;
 }
 
 }
--- algorithms/MostOccuringItemTest.cpp
+++ algorithms/MostOccuringItemTest.cpp
@@ -11,9 +11,13 @@ TEST_CASE("most occuring", "[count]")
     CHECK(count::MostOccuringItem(std::vector<int>{ 0, 1, 1, 3, 5, 5 }) == 5);
     CHECK(count::MostOccuringItem(std::vector<int>{ 0, 1, 1, 3, 5, 5, 1 }) == 1);
 
-    CHECK(count::MostOccuringItem(std::list<int>{ 1 }) == 1);
-    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1 }) == 1);
-    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1, 1, 3, 5 }) == 1);
-    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1, 1, 3, 5, 5 }) == 5);
-    CHECK(count::MostOccuringItem(std::list<int>{ 0, 1, 1, 3, 5, 5, 1 }) == 1);
+    CHECK(count::MostOccuringItemSort(std::vector<int>{ 1 }) == 1);
+    CHECK(count::MostOccuringItemSort(std::vector<int>{ 0, 1 }) == 1);
+    CHECK(count::MostOccuringItemSort(std::vector<int>{ 0, 1, 1, 3, 5 }) == 1);
+    CHECK(count::MostOccuringItemSort(std::vector<int>{ 0, 1, 1, 3, 5, 5 }) == 5);
+    CHECK(count::MostOccuringItemSort(std::vector<int>{ 0, 1, 1, 3, 5, 5, 1 }) == 1);
+
+    //BENCHMARK_ADVANCED("benchmark")(Catch::Benchmark::Chronometer meter) {
+        //meter.measure([]{  });
+    //};
 }
