3508ba5b9f9fd6e3c2dd8307b6ec878123440f5a
Juhwan Song <jsong785@gmail.com>
2019-12-08 13:41:29 -0700

    functional changes

--- functional/currying.cpp
+++ functional/currying.cpp
@@ -12,9 +12,22 @@ auto currying(const int x) {
     };
 }
 
+template <typename Func, typename FuncArg>
+auto GetCurryFunc(Func&& f(FuncArg &&arg))
+{
+    return [=](){
+    };
+}
+
+template <typename Func, typename Arg1, typename ...Args>
+auto GetCurryFunc(Func f(Arg1 arg1, Args ...args))
+{
+}
+
 TEST_CASE("currying", "")
 {
     CHECK(original(1, 2, 3) == currying(1)(2)(3));
     CHECK(original(1, 1, 1) == currying(1)(1)(1));
+    //CHECK(original(1, 2, 3) == GetCurryFunc(original)(1)(2)(3));
 }
 
