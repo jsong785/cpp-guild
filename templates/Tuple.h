be5b4c940f2e4dd1341644aa79f2f7f13072ae63
Juhwan Song <jsong785@gmail.com>
2020-02-01 11:23:19 -0700

    changes

--- templates/Tuple.h
+++ templates/Tuple.h
@@ -12,13 +12,13 @@ template <std::size_t Index, typename Callable, typename ...Args>
 std::enable_if_t<Index < std::tuple_size<std::tuple<Args...>>::value, void> TransformTupleHelper(std::tuple<Args...> &tuple, Callable &&c)
 {
     std::get<Index>(tuple) = c(std::get<Index>(tuple));
-    TransformTupleHelper<Index+1>(tuple, c);
+    TransformTupleHelper<Index+1>(tuple, std::forward<Callable>(c));
 }
 
 
 template <typename Callable, typename ...Args>
 void TransformTuple(std::tuple<Args...> &tuple, Callable &&c)
 {
-    TransformTupleHelper<0>(tuple, c);
+    TransformTupleHelper<0>(tuple, std::forward<Callable>(c));
 }
 
