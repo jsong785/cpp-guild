77b17e3a6c2cec36a0d376edc298cbc306a032fc
Juhwan Song <jsong785@gmail.com>
2020-03-03 23:49:21 -0700

    using clang, formatted system, lost some configuration files

--- templates/Tuple.h
+++ templates/Tuple.h
@@ -3,22 +3,23 @@
 #include <tuple>
 #include <type_traits>
 
-template <std::size_t Index, typename Callable, typename ...Args>
-std::enable_if_t<Index >= std::tuple_size<std::tuple<Args...>>::value, void> TransformTupleHelper(std::tuple<Args...> &tuple, Callable &&c)
-{
-}
+namespace details {
 
 template <std::size_t Index, typename Callable, typename ...Args>
-std::enable_if_t<Index < std::tuple_size<std::tuple<Args...>>::value, void> TransformTupleHelper(std::tuple<Args...> &tuple, Callable &&c)
+void TransformTupleHelper(std::tuple<Args...> &tuple, Callable c)
 {
-    std::get<Index>(tuple) = c(std::get<Index>(tuple));
-    TransformTupleHelper<Index+1>(tuple, std::forward<Callable>(c));
+    if constexpr(Index < std::tuple_size<std::tuple<Args...>>::value) {
+	std::get<Index>(tuple) = c(std::get<Index>(tuple));
+	TransformTupleHelper<Index+1>(tuple, std::forward<Callable>(c));
+    }
+}
+
 }
 
 
 template <typename Callable, typename ...Args>
-void TransformTuple(std::tuple<Args...> &tuple, Callable &&c)
+void TransformTuple(std::tuple<Args...> &tuple, Callable c)
 {
-    TransformTupleHelper<0>(tuple, std::forward<Callable>(c));
+	details::TransformTupleHelper<0>(tuple, std::move(c));
 }
 
