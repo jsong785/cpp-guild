89678c3a097d7d330cf321e495d1b05301346977
Juhwan Song <jsong785@gmail.com>
2020-03-08 22:35:30 -0600

    maybe monad

--- functional/maybe_monad.cpp
+++ functional/maybe_monad.cpp
@@ -3,4 +3,7 @@
 
 TEST_CASE("Maybe Monad", "[monad]")
 {
+    CHECK_FALSE(MaybeMonad<int>::Construct());
+    CHECK(MaybeMonad<int>::Unit(4));
+    CHECK(*MaybeMonad<int>::Unit(4) == 4);
 }
--- functional/maybe_monad.hpp
+++ functional/maybe_monad.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <optional>
+
+template <typename T>
+struct MaybeMonad
+{
+    static std::optional<T> Construct() {
+        return {};
+    }
+
+    static std::optional<T> Unit(T &&t) {
+        return std::optional<T>{ std::forward<decltype(t)>(t) };
+    }
+
+};
+
