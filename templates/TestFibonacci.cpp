613cfd598e197ebe8a6e2d61b60a22b3facb330e
Juhwan Song <jsong785@gmail.com>
2020-03-08 19:31:30 -0600

    alternate method

--- templates/TestFibonacci.cpp
+++ templates/TestFibonacci.cpp
@@ -9,3 +9,15 @@ static_assert(CalculateFibonacci(5) == 5);
 static_assert(CalculateFibonacci(6) == 8);
 static_assert(CalculateFibonacci(7) == 13);
 static_assert(CalculateFibonacci(8) == 21);
+
+
+static_assert(FibonacciStruct<0>::value_type == 0);
+static_assert(FibonacciStruct<1>::value_type == 1);
+static_assert(FibonacciStruct<2>::value_type == 1);
+static_assert(FibonacciStruct<3>::value_type == 2);
+static_assert(FibonacciStruct<4>::value_type == 3);
+static_assert(FibonacciStruct<5>::value_type == 5);
+static_assert(FibonacciStruct<6>::value_type == 8);
+static_assert(FibonacciStruct<7>::value_type == 13);
+static_assert(FibonacciStruct<8>::value_type == 21);
+
--- templates/fibonacci.h
+++ templates/fibonacci.h
@@ -11,3 +11,18 @@ constexpr int64_t CalculateFibonacci(const int64_t sequence) {
     return CalculateFibonacci(sequence-1) + CalculateFibonacci(sequence-2);
 }
 
+template <std::size_t N>
+struct FibonacciStruct {
+    static const std::size_t value_type{ FibonacciStruct<N-1>::value_type + FibonacciStruct<N-2>::value_type };
+};
+
+template <>
+struct FibonacciStruct<0> {
+    static const std::size_t value_type{ 0 };
+};
+
+template <>
+struct FibonacciStruct<1> {
+    static const std::size_t value_type{ 1 };
+};
+
