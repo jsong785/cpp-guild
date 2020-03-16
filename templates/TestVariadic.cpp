b31690d391a3096deba86133be21e577a9673df8
Juhwan Song <jsong785@gmail.com>
2020-03-15 21:28:04 -0600

    filter

--- templates/TestVariadic.cpp
+++ templates/TestVariadic.cpp
@@ -6,4 +6,7 @@ TEST_CASE("variadic", "[variadic]")
     CHECK(ConcatStream(1, 1, 1, 1, 1).str() == "11111");
     CHECK(ConcatStream(1, "1", "one", 1.1).str() == "11one1.1");
     CHECK(ConcatDoubleStream(1, 1, 1).str() == "222");
+
+    CHECK(Filter<int>({1, 2, 3, 4}, 0, 1) == std::vector<int>{1, 2});
+    CHECK(Filter<int>({1, 2, 3, 4}, 3, 2) == std::vector<int>{4, 3});
 }
--- templates/variadic.hpp
+++ templates/variadic.hpp
@@ -13,3 +13,23 @@ template <typename... T>
 std::stringstream ConcatDoubleStream(T... args) {
     return ConcatStream((args+args)...);
 }
+
+namespace details {
+
+template <typename A, typename Item, typename... Items>
+void FilterHelper(A& retVector, Item item, Items... items) {
+    retVector.emplace_back(std::move(item));
+    if constexpr(sizeof...(items) > 0) {
+        FilterHelper(retVector, items...);
+    }
+}
+
+}
+
+template <typename T, typename... Indices>
+std::vector<T> Filter(const std::vector<T> &list, Indices... indices) {
+    std::vector<T> ret;
+    ret.reserve( sizeof...(indices) );
+    details::FilterHelper(ret, list[indices]...);
+    return ret;
+}
