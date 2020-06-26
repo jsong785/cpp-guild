6d46f80a702f219daf2ca6e596887b4eb74ee8ca
Juhwan Song <jsong785@gmail.com>
2020-06-26 00:45:06 -0600

    monads

--- functional/monads.cpp
+++ functional/monads.cpp
@@ -1,6 +1,67 @@
 #include "catch.hpp"
 #include "monads.hpp"
 
-TEST("Maybe", "[monads]")
+#include <string>
+
+TEST_CASE("Maybe", "[monads]")
+{
+    SECTION("Just")
+    {
+        static_assert(Just(1) == Maybe<int>{ 1 });
+        CHECK(Just(std::string{ "something" }) == Maybe<std::string>{ "something" });
+    }
+
+    SECTION("Nothing")
+    {
+        static_assert(Nothing<int>() == Maybe<int>{});
+        CHECK(Nothing<std::string>() == Maybe<std::string>{});
+    }
+
+    constexpr auto addOne = [](const int x) -> Maybe<int> {
+        return x + 1;
+    };
+    const auto toString = [](const auto x) -> Maybe<std::string> {
+        return std::to_string(x);
+    };
+
+    SECTION("MBind")
+    {
+        CHECK(Mbind(Just(10), addOne) == Just(11));
+        CHECK(Mbind(Nothing<int>(), addOne) == Nothing<int>());
+
+        CHECK(Mbind(Just(123), toString) == Just(std::string{ "123" }));
+        CHECK(Mbind(Nothing<int>(), toString) == Nothing<std::string>());
+        static_assert(Mbind(Nothing<int>(), addOne) == Nothing<int>());
+    }
+
+    SECTION("Chaining")
+    {
+        CHECK((Just(11) | addOne | toString) == Just(std::string{ "12" }));
+        CHECK((Just(100) | addOne | toString) == Just(std::string{ "101" }));
+
+        CHECK((Nothing<int>() | addOne | toString) == Nothing<std::string>());
+        static_assert((Nothing<int>() | addOne) == Nothing<int>());
+    }
+}
+
+TEST_CASE("List", "[monads]")
+{
+    const auto doubleEntry = [](const auto x) -> List<int> {
+        return { x, x };
+    };
+
+    const auto toString = [](const auto x) -> List<std::string> {
+        return { std::to_string(x) };
+    };
+
+    CHECK((List<int>{1, 2, 3} | doubleEntry) == List<int>{ 1, 1, 2, 2, 3, 3 });
+    CHECK((List<int>{1, 2, 3} | doubleEntry | toString) == 
+            List<std::string>{ "1", "1", "2", "2", "3", "3" });
+
+    CHECK((List<int>{} | doubleEntry | toString) == List<std::string>{});
+}
+
+TEST_CASE("Continuation", "[monads]")
 {
 }
+
--- functional/monads.hpp
+++ functional/monads.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <optional>
+#include <vector>
+
+template <typename A, typename Func>
+constexpr auto operator|(A, Func);
 
 template <typename T>
 using Maybe = std::optional<T>;
 
 template <typename T>
 constexpr Maybe<T> Just(T t) {
-    return std::move(t);
+    return { std::move(t) };
 }
 
 template <typename T>
@@ -16,9 +20,37 @@ constexpr Maybe<T> Nothing() {
 }
 
 template <typename A, typename Func>
-constexpr auto operator|(Maybe<A> ma, Func f) {
+constexpr auto Mbind(Maybe<A> ma, Func f) -> std::invoke_result_t<Func, A> {
+    using ReturnType = std::invoke_result_t<Func, A>;
+    static_assert(std::is_same_v<ReturnType, Maybe<typename ReturnType::value_type>>);
     if(ma) {
+        return std::invoke(f, std::move(*ma));
     }
     return {};
+}
+
+template <typename T>
+using List = std::vector<T>;
+
+template <typename A, typename Func>
+constexpr auto Mbind(List<A> ma, Func f) -> std::invoke_result_t<Func, A> {
+    using ReturnType = std::invoke_result_t<Func, A>;
+    static_assert(std::is_same_v<ReturnType, List<typename ReturnType::value_type>>);
+
+    ReturnType other;
+    if(!ma.empty()) {
+        other.reserve(ma.size());
+
+        for(auto&& m : ma) {
+            auto res = std::invoke(f, decltype(m)(m));
+            other.insert(other.end(), std::make_move_iterator(res.begin()), std::make_move_iterator(res.end()));
+        }
+    }
+    return other;
+}
+
+template <typename A, typename Func>
+constexpr auto operator|(A ma, Func f) {
+    return Mbind(std::move(ma), std::move(f));
 }
 
