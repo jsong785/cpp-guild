b322b6ee4eaf4aef4192f74fae83cd603d255082
Juhwan Song <jsong785@gmail.com>
2020-06-26 13:17:46 -0600

    deleting files

--- functional/maybe_monad.cpp
+++
@@ -1,9 +0,0 @@
-#include "catch.hpp"
-#include "maybe_monad.hpp"
-
-TEST_CASE("Maybe Monad", "[monad]")
-{
-    CHECK_FALSE(MaybeMonad<int>::Construct());
-    CHECK(MaybeMonad<int>::Unit(4));
-    CHECK(*MaybeMonad<int>::Unit(4) == 4);
-}
