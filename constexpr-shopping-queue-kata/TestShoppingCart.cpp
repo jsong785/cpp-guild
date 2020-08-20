6d325944832d9d65a3e40ff946da336be93a767e
Juhwan Song <jsong785@gmail.com>
2020-08-19 20:57:45 -0600

    shopping cart kata

---
+++ constexpr-shopping-queue-kata/TestShoppingCart.cpp
@@ -0,0 +1,168 @@
+#include "enum_flags.hpp"
+#include <cstdint>
+
+template <typename T>
+void TestInitialization()
+{
+    // initialization (enum only)
+    static_assert(Flags<T>{T::A, T::B} == Flags<T>{}.Set(T::A, T::B));
+    static_assert(Flags<T>{T::A, T::B} != Flags<T>{}.Set(T::A));
+    static_assert(Flags<T>{T::A, T::B, T::C, T::D} == Flags<T>{}.Set(T::ALL));
+
+    // initialization (flags only)
+    static_assert(Flags<T>{Flags<T>{T::A}, Flags<T>{T::B}} == Flags<T>{}.Set(T::A, T::B));
+    static_assert(Flags<T>{Flags<T>{T::A}, Flags<T>{T::B}} != Flags<T>{}.Set(T::A));
+    static_assert(Flags<T>{Flags<T>{T::A}, Flags<T>{T::B}, Flags<T>{T::C}, Flags<T>{T::D}} == Flags<T>{}.Set(T::ALL));
+
+    // initialization (mixed)
+    static_assert(Flags<T>{Flags<T>{T::A}, T::B} == Flags<T>{}.Set(T::A, T::B));
+    static_assert(Flags<T>{T::A, Flags<T>{T::B}} != Flags<T>{}.Set(T::A));
+    static_assert(Flags<T>{Flags<T>{T::A}, T::B, Flags<T>{T::C}, Flags<T>{T::D}} == Flags<T>{}.Set(T::ALL));
+}
+
+template <typename T>
+void TestTest()
+{
+    // Test (enum only)
+    static_assert(Flags<T>{T::A, T::B}.Test(T::A));
+    static_assert(!Flags<T>{T::A, T::B}.Test(T::C));
+    static_assert(!Flags<T>{T::A, T::B}.Test(T::ALL));
+    static_assert(Flags<T>{T::ALL}.Test(T::ALL));
+    static_assert(Flags<T>{T::ALL}.Test(T::C));
+
+    // Test (flags only)
+    static_assert(Flags<T>{T::A, T::B}.Test(Flags<T>{T::A}));
+    static_assert(!Flags<T>{T::A, T::B}.Test(Flags<T>{T::C}));
+    static_assert(!Flags<T>{T::A, T::B}.Test(Flags<T>{T::ALL}));
+    static_assert(Flags<T>{T::ALL}.Test(Flags<T>{T::ALL}));
+    static_assert(Flags<T>{T::ALL}.Test(Flags<T>{T::C}));
+}
+
+template <typename T>
+void TestSet()
+{
+    // Set (enum only)
+    static_assert(Flags<T>{T::A}.Set(T::A) == Flags<T>{T::A});
+    static_assert(Flags<T>{}.Set(T::A, T::B) == Flags<T>{T::A, T::B});
+    static_assert(Flags<T>{}.Set(T::ZERO) == Flags<T>{T::ZERO});
+    static_assert(Flags<T>{}.Set(T::A).Set(T::B, T::C, T::D) == Flags<T>{T::ALL});
+
+    // Set (flags only)
+    static_assert(Flags<T>{T::A}.Set(Flags<T>{T::A}) == Flags<T>{T::A});
+    static_assert(Flags<T>{}.Set(Flags<T>{T::A, T::B}) == Flags<T>{T::A, T::B});
+    static_assert(Flags<T>{}.Set(Flags<T>{T::ZERO}) == Flags<T>{T::ZERO});
+    static_assert(Flags<T>{}.Set(Flags<T>{T::A}).Set(Flags<T>{T::B, T::C, T::D}) == Flags<T>{T::ALL});
+
+    // Set (mixed)
+    static_assert(Flags<T>{}.Set(Flags<T>{T::A}).Set(T::B).Set(Flags<T>{T::C, T::D}) == Flags<T>{T::ALL});
+}
+
+template <typename T>
+void TestUnset()
+{
+    // Unset (enum only)
+    static_assert(Flags<T>{T::A, T::B}.Unset(T::A) == Flags<T>{T::B});
+    static_assert(Flags<T>{T::A, T::B}.Unset(T::A, T::B) == Flags<T>{});
+    static_assert(Flags<T>{T::A, T::B}.Unset(T::A, T::B) == Flags<T>{T::ZERO});
+    static_assert(Flags<T>{T::A, T::B}.Unset(T::ALL) == Flags<T>{});
+
+    // Unset (Flags only)
+    static_assert(Flags<T>{T::A, T::B}.Unset(Flags<T>{T::A}) == Flags<T>{T::B});
+    static_assert(Flags<T>{T::A, T::B}.Unset(Flags<T>{T::A, T::B}) == Flags<T>{});
+    static_assert(Flags<T>{T::A, T::B}.Unset(Flags<T>{T::A, T::B}) == Flags<T>{T::ZERO});
+    static_assert(Flags<T>{T::A, T::B}.Unset(Flags<T>{T::ALL}) == Flags<T>{});
+
+    // Unset (mixed)
+    static_assert(Flags<T>{T::A, T::B}.Unset(Flags<T>{T::A}, T::B) == Flags<T>{T::ZERO});
+    static_assert(Flags<T>{T::A, T::B}.Unset(T::A, Flags<T>{T::B}) == Flags<T>{T::ZERO});
+}
+
+template <typename T>
+void TestFlip()
+{
+    // Flip
+    static_assert(Flags<T>{T::A, T::B}.Flip() != Flags<T>{T::A, T::B});
+    static_assert(!Flags<T>{T::A, T::B}.Flip().Test(T::A));
+    static_assert(!Flags<T>{T::A, T::B}.Flip().Test(T::B));
+    static_assert(Flags<T>{T::A, T::B}.Flip().Test(T::C));
+
+    static_assert(Flags<T>{T::A, T::B}.Flip().Flip() == Flags<T>{T::A, T::B});
+    static_assert(Flags<T>{T::A, T::B}.Flip().Flip().Test(T::A));
+    static_assert(Flags<T>{T::A, T::B}.Flip().Flip().Test(T::B));
+    static_assert(!Flags<T>{T::A, T::B}.Flip().Flip().Test(T::C));
+}
+
+template <typename T>
+void TestOperators()
+{
+    // operator&
+    static_assert((Flags<T>{T::A, T::B} & Flags<T>{T::A, T::B}) == Flags<T>{T::A, T::B});
+    static_assert((Flags<T>{T::A} & Flags<T>{T::A, T::B}) == Flags<T>{T::A});
+    static_assert((Flags<T>{T::A} & Flags<T>{T::B}) == Flags<T>{});
+
+    // operator |
+    static_assert((Flags<T>{T::A, T::B} | Flags<T>{T::A, T::B}) == Flags<T>{T::A, T::B});
+    static_assert((Flags<T>{T::A} | Flags<T>{T::A, T::B}) == Flags<T>{T::A, T::B});
+    static_assert((Flags<T>{T::A} | Flags<T>{T::B}) == Flags<T>{T::A, T::B});
+
+    // operator ^
+    static_assert((Flags<T>{T::A, T::B} ^ Flags<T>{T::A, T::B}) == Flags<T>{});
+    static_assert((Flags<T>{T::A} ^ Flags<T>{T::A, T::B}) == Flags<T>{T::B});
+    static_assert((Flags<T>{T::A} ^ Flags<T>{T::B}) == Flags<T>{T::A, T::B});
+}
+
+inline static void ConstexprTests() {
+    enum class TestRegular
+    {
+        ZERO = 0,
+        A = 1 << 0,
+        B = 1 << 1,
+        C = 1 << 2,
+        D = 1 << 3,
+        ALL = 0xF
+    };
+
+    enum class TestInt8 : int8_t {
+        ZERO = 0,
+        A = 1 << 0,
+        B = 1 << 1,
+        C = 1 << 2,
+        D = 1 << 3,
+        ALL = 0xF
+    };
+
+    enum class TestUint8 : uint8_t {
+        ZERO = 0,
+        A = 1 << 0,
+        B = 1 << 1,
+        C = 1 << 2,
+        D = 1 << 3,
+        ALL = 0xF
+    };
+
+    TestInitialization<TestRegular>();
+    TestTest<TestRegular>();
+    TestSet<TestRegular>();
+    TestUnset<TestRegular>();
+    TestFlip<TestRegular>();
+    TestOperators<TestRegular>();
+
+    TestInitialization<TestInt8>();
+    TestTest<TestInt8>();
+    TestSet<TestInt8>();
+    TestUnset<TestInt8>();
+    TestFlip<TestInt8>();
+    TestOperators<TestInt8>();
+
+    TestInitialization<TestUint8>();
+    TestTest<TestUint8>();
+    TestSet<TestUint8>();
+    TestUnset<TestUint8>();
+    TestFlip<TestUint8>();
+    TestOperators<TestUint8>();
+}
+
+int main(const int, const char**)
+{
+    return 0;
+}
---
+++ constexpr-shopping-queue-kata/shopping_cart.hpp
@@ -0,0 +1,148 @@
+#pragma once
+
+#include <type_traits>
+
+template <typename Enum>
+class Flags
+{
+    public:
+        constexpr explicit Flags() noexcept = default;
+
+        template <typename... Args>
+        constexpr explicit Flags(Args... args) noexcept {
+            Set(args...);
+        }
+
+        constexpr bool IsEqual(const Flags& other) const noexcept {
+            return this->m_flags == other.m_flags;
+        }
+
+        template <typename T>
+        friend constexpr bool operator==(const Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        template <typename T>
+        friend constexpr bool operator!=(const Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        template <typename T>
+        friend constexpr Flags<T> operator&(const Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        template <typename T>
+        friend constexpr Flags<T> operator|(const Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        template <typename T>
+        friend constexpr Flags<T> operator^(const Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        template <typename T>
+        friend constexpr Flags<T>& operator&=(Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        template <typename T>
+        friend constexpr Flags<T>& operator|=(Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        template <typename T>
+        friend constexpr Flags<T>& operator^=(Flags<T>& f1, const Flags<T>& f2) noexcept;
+
+        constexpr auto& Set(const Enum e) noexcept {
+            this->m_flags |= static_cast<EnumFlag>(e);
+            return *this;
+        }
+
+        constexpr auto& Set(const Flags f) noexcept {
+            this->m_flags |= f.m_flags;
+            return *this;
+        }
+
+        template <typename... Args>
+        constexpr auto& Set(Args... args) noexcept {
+            static_assert(((std::is_same_v<Enum, Args> || std::is_same_v<Flags<Enum>, Args>) || ...));
+            return (this->Set(args), ...);
+        }
+
+        constexpr auto& Unset(const Enum e) noexcept {
+            this->m_flags &= ~static_cast<EnumFlag>(e);
+            return *this;
+        }
+
+        constexpr auto& Unset(const Flags f) noexcept {
+            this->m_flags &= ~f.m_flags;
+            return *this;
+        }
+
+        template <typename... Args>
+        constexpr auto& Unset(Args... args) noexcept {
+            static_assert(((std::is_same_v<Enum, Args> || std::is_same_v<Flags<Enum>, Args>) || ...));
+            return (this->Unset(args), ...);
+        }
+
+        constexpr auto& Flip() noexcept {
+            this->m_flags = ~this->m_flags;
+            return *this;
+        }
+
+        constexpr bool Test(const Enum e) const noexcept {
+            return (m_flags & static_cast<EnumFlag>(e)) == static_cast<EnumFlag>(e);
+        }
+
+        constexpr bool Test(const Flags f) const noexcept {
+            return (m_flags & f.m_flags) == f.m_flags;
+        }
+
+    private:
+        static_assert(std::is_enum_v<Enum>);
+        using EnumFlag = std::underlying_type_t<Enum>;
+        EnumFlag m_flags{};
+};
+
+template <typename Enum>
+constexpr bool operator==(const Flags<Enum>& f1, const Flags<Enum>& f2) noexcept
+{
+    return f1.IsEqual(f2);
+}
+
+template <typename Enum>
+constexpr bool operator!=(const Flags<Enum>& f1, const Flags<Enum>& f2) noexcept
+{
+    return !f1.IsEqual(f2);
+}
+
+template <typename T>
+constexpr Flags<T> operator&(const Flags<T>& f1, const Flags<T>& f2) noexcept
+{
+    auto copy = f1;
+    return copy &= f2;
+}
+
+template <typename T>
+constexpr Flags<T> operator|(const Flags<T>& f1, const Flags<T>& f2) noexcept
+{
+    auto copy = f1;
+    return copy |= f2;
+}
+
+template <typename T>
+constexpr Flags<T> operator^(const Flags<T>& f1, const Flags<T>& f2) noexcept
+{
+    auto copy = f1;
+    return copy ^= f2;
+}
+
+template <typename T>
+constexpr Flags<T>& operator&=(Flags<T>& f1, const Flags<T>& f2) noexcept
+{
+    f1.m_flags &= f2.m_flags;
+    return f1;
+}
+
+template <typename T>
+constexpr Flags<T>& operator|=(Flags<T>& f1, const Flags<T>& f2) noexcept
+{
+    f1.m_flags |= f2.m_flags;
+    return f1;
+}
+
+template <typename T>
+constexpr Flags<T>& operator^=(Flags<T>& f1, const Flags<T>& f2) noexcept
+{
+    f1.m_flags ^= f2.m_flags;
+    return f1;
+}
+
