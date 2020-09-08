fec457f7bd08175e2085dca0b501a7604fe96266
Juhwan Song <jsong785@gmail.com>
2020-09-07 22:16:17 -0600

    dfs recursive

--- algorithms/Graph.hpp
+++ algorithms/Graph.hpp
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <vector>
+
+using Visited = std::vector<std::size_t>;
+using AdjList = std::vector<std::vector<std::size_t>>;
+
+Visited DfsHelper(const AdjList& graph, const std::size_t node, std::vector<bool>& visitedCache) {
+    if(visitedCache[node]) {
+        return {};
+    }
+    visitedCache[node] = true;
+    Visited visited{ node };
+
+    const auto& neighbors = graph[node];
+    for(const auto n : neighbors) {
+        auto v = DfsHelper(graph, n, visitedCache);
+        visited.insert(visited.end(), v.cbegin(), v.cend()); // POD, so move doesn't matter
+    }
+    return visited;
+}
+
+Visited Dfs(const AdjList& graph, const std::size_t node) 
+{
+    std::vector<bool> visitedCache(graph.size());
+    return DfsHelper(graph, node, visitedCache);
+}
+
--- algorithms/GraphTest.cpp
+++ algorithms/GraphTest.cpp
@@ -1,2 +1,8 @@
 #include "Graph.hpp"
 #include "catch.hpp"
+
+TEST_CASE("Graph - Dfs", "[graph]")
+{
+    CHECK(Dfs(AdjList{{1, 2}, {2}, {0, 3}, {3}}, 1) == Visited{1, 2, 0, 3});
+    CHECK(Dfs(AdjList{{2, 1}, {2, 3}, {0}, {3}}, 2) == Visited{2, 0, 1, 3});
+}
